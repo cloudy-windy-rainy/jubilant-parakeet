96f44dcb2211201572d99145f6be96ed23f7bfa0
cloudy-windy-rainy <96875233+cloudy-windy-rainy@users.noreply.github.com>
2021-12-31 16:21:39 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
 print('hello world')
+
+
+def half_adder(A,B):
+    half_out = A^B
+    carry = A&B
+    return half_out, carry
+
+print(' A B  S  C ')
+print(' 0 0',half_adder(0,0))
+print(' 0 0',half_adder(0,1))
+print(' 0 0',half_adder(1,0))
+print(' 0 0',half_adder(1,1))
